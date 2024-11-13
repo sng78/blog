766e92a2fdf29005edcf61f5bd61f91457554be7
Serge Gorbachev <sng78@list.ru>
2024-11-13 09:43:58 +0300

    add blog-details

--- src/main/java/io/github/sng78/blog/web/article/ArticleController.java
+++ src/main/java/io/github/sng78/blog/web/article/ArticleController.java
@@ -5,8 +5,12 @@ import io.github.sng78.blog.repository.ArticleRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class ArticleController {
@@ -35,5 +39,18 @@ public class ArticleController {
     public String blogArticleAdd(@RequestParam String title, @RequestParam String preview, @RequestParam String content) {
         repository.save(new Article(title, content, preview));
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String articleDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!repository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        model.addAttribute("title", "Блог сайта");
+        Optional<Article> article = repository.findById(id);
+        ArrayList<Article> result = new ArrayList<>();
+        article.ifPresent(result::add);
+        model.addAttribute("article", result);
+        return "blog-details";
     }
 }
